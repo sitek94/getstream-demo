7b35e37fe1b606079ae2f81db01697c3371f705e
Maciek <msitkowski94@gmail.com>
2022-04-30 05:46:20 +0200

    init express server

---
+++ db/users.ts
@@ -0,0 +1,23 @@
+type User = {
+  id: string
+  name: string
+}
+
+export const users: User[] = [
+  {
+    id: 'id-maciek',
+    name: 'Maciek',
+  },
+  {
+    id: 'id-beethoven',
+    name: 'Beethoven',
+  },
+  {
+    id: 'id-mozart',
+    name: 'Mozart',
+  },
+  {
+    id: 'id-chopin',
+    name: 'Chopin',
+  },
+]
